c7e398538dcec2ef28c2fa0fd73adc68bd2a8942
Chamindu Vidyarathne <chamindudvidyarathne@gmail.com>
2026-01-25 00:22:05 +0530

    Add logger

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,13 @@
 #include "Game.h"
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <iostream>
+#include <spdlog/spdlog.h>
 
 Game::Game()
-    : m_window(sf::VideoMode(sf::Vector2u(1280, 720)), "Algorithmic Arena") {}
+    : m_window(sf::VideoMode(sf::Vector2u(1280, 720)), "Algorithmic Arena") {
+  spdlog::info("Game Initialized");
+}
 
 void Game::run() {
   while (m_window.isOpen()) {
@@ -15,12 +19,13 @@ void Game::run() {
 void Game::processEvents() {
   while (const auto event = m_window.pollEvent()) {
     if (event->is<sf::Event::Closed>()) {
+      spdlog::warn("Window close event detected");
       m_window.close();
     }
   }
 }
 
 void Game::render() {
-  m_window.clear(sf::Color::Cyan);
+  m_window.clear(sf::Color::White);
   m_window.display();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "Game.h"
+#include <spdlog/spdlog.h>
 
 int main() {
+
+  spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%^%l%$] %v");
 
   Game game;
   game.run();
